93453723d5c946a2b96af46fd6f6e0b4dce469c3
Lonozx <lonozx@gmail.com>
2023-01-04 23:28:58 +0200

    unit-5

---
+++ js-lesson-4/main.js
@@ -0,0 +1,16 @@
+// let container = document.querySelector('.container');
+// let name = "Sasha";
+// // for(let i=1;i<=10;i++){
+// //     container.innerHTML += `${name} <br>`;
+// // }
+
+// let inp1 = document.querySelector('.input');
+// let out = document.querySelector('.out');
+// let btn = document.querySelector('button').onclick = () => {
+//     if(+inp1.value!=0 && +inp1.value!=null){
+//     for(let i=1; i<= +inp1.value;i++){
+//         out.innerHTML += `${name} + <br>`;
+//     }   
+// } else console.log("error");
+// };
+
