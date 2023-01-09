fc7ac3105f0658836740a38ca942116feedf0c2a
Lonozx <lonozx@gmail.com>
2023-01-09 23:19:57 +0200

    2nd

--- js-lesson-6/homework/main.js
+++ js-lesson-6/homework/main.js
@@ -2,11 +2,21 @@ let out = document.querySelector('.out');
 let out2 = document.querySelector('.out2');
 let out3 = document.querySelector('.out3');
 let out4 = document.querySelector('.out4');
+let out5 = document.querySelector('.out5');
+let out6 = document.querySelector('.out6');
 let riders = document.querySelector('.rider');
 let inputW = document.querySelector('.inp-wid');
 let addButton = document.querySelector('.Add');
 let checkButton = document.querySelector('.check');
 let widButton = document.querySelector('.wid');
+let timeSetter = document.querySelector('.timeSetter');
+let hours = document.querySelector('.hours');
+let minutes = document.querySelector('.minutes');
+let seconds = document.querySelector('.seconds');
+let select = document.querySelectorAll('option');
+let floatButton = document.querySelector('.float');
+let floatInput1 = document.querySelector('.float1');
+let floatInput2 = document.querySelector('.float2');
 let car = {
     manufacturer: 'Toyota',
     model: 'Camry',
@@ -59,3 +69,104 @@ widButton.onclick = () => {
     let a = car.count(inputW.value);
     out4.innerHTML = a;
 }
+
+let timeSpace = {
+    hours: 24,
+    minutes: 60,
+    seconds: 60,
+    display: function(){
+        out5.innerHTML = timeSpace.hours + ':' + timeSpace.minutes + ':' + timeSpace.seconds;
+    },
+    hoursChange: function(x){
+        if(x<=24){
+        this.hours = x;
+        console.log(x);
+        console.log(this.hours);
+    }
+        else {this.hours=x-24;}
+        return this.hours;
+    },
+    minutesChange: function(x){
+        if(x<60){this.minutes = x;
+        } else if(x>60){
+            this.hours++;
+        }
+        if(this.minutes==0){
+            this.minutes+='0';
+        }
+        return this.minutes;
+    },
+    secondsChange: function(x){
+        if(x<60){this.seconds = x;
+        } else if(x>60){
+            this.minutes++;
+        }
+        if(this.seconds==0){
+            this.seconds+='0';
+        }
+        return this.seconds;
+    },
+}
+timeSpace.display();
+
+timeSetter.onclick = () =>{
+    if(hours.value!=0 && minutes.value!=0 && seconds.value!=0){
+   let a =  timeSpace.hoursChange(hours.value);
+   let b = timeSpace.minutesChange(minutes.value);
+   let c = timeSpace.secondsChange(seconds.value);
+   out5.innerHTML = a + ':' + b + ':' + c;
+} 
+else out5.innerHTML = '<b>Please enter whole numbuz!</b>';
+    hours.value='';
+    minutes.value='';
+    seconds.value='';
+}
+
+let floatin = {
+    upper: 1.4,
+    down: 1.6,
+    summary: function(x, y){
+        return x+y;
+    },
+    minus: function(x, y){
+        return x-y;
+    },
+    multi: function(x, y){
+        return x*y;
+    },
+    division: function(x, y){
+        return x/y;
+    },
+    divisionExe: function(x, y){
+        return x%y;
+    },
+}
+    select.onchange = () =>{
+    floatInput1.value='';
+    floatInput2.value='';
+    let summary = floatin.summary(floatInput1, floatInput2);
+    let minus = floatin.minus(floatInput1, floatInput2);
+    let multi = floatin.multi(floatInput1, floatInput2);
+    let division = floatin.division(floatInput1, floatInput2);
+    let divisionExe = floatin.divisionExe(floatInput1, floatInput2);
+    if(option.value == '+'){
+        out6.innerHTML = summary;
+    }
+}
+    if(option.minus){
+        out6.innerHTML = minus;
+    }
+
+    if(option.multi){
+        out6.innerHTML = multi;
+    }
+
+    if(option.division){
+        out6.innerHTML = division;
+    }
+
+    if(option.divisionExe){
+        out6.innerHTML = divisionExe;
+    }
+
+  
